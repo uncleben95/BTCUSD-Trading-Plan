f9e975ab2b3cf34b5570b147d816f720ee5c9064
uncleben95 <uncleben95@gmail.com>
2026-09-10 10:53:36 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='btc-intel-v3';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/index.html','/manifest.json']))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{ if(e.request.method!=='GET') return; e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const copy=r.clone(); caches.open(CACHE).then(x=>x.put(e.request,copy)); return r;}).catch(()=>caches.match('/'))); });
